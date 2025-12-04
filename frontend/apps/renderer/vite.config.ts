6adf747ae50844b50ffbddfe8206a47b6374b6cb
heejung <gimm9567@gmail.com>
2025-12-05 01:33:48 +0900

    feat: modify dependency

--- frontend/apps/renderer/vite.config.ts
+++ frontend/apps/renderer/vite.config.ts
@@ -3,11 +3,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({ 
+  base: './',
   plugins: [react()],
   server: { port: 5173 },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
   },
 })
